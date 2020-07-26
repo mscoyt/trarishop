23108b610f39d4b00505dfae9d7148ba3b956629
mscoyt <67257841+mscoyt@users.noreply.github.com>
2020-07-27 00:06:24 +0400

    Add files via upload

---
+++ trari/css/app.js
@@ -0,0 +1,11 @@
+$(function(){
+  
+  $("[data-collapse]").on("click", function(event)){
+	  event.preventDefault();
+	  
+	  var $this = $(this),
+	     blockId = $this.data(collapse);
+		 
+	  $this.toggleClass("active");
+	  $(blockId).slideToggle();
+  });
